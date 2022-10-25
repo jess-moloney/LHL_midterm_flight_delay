938c192c4ac9c246e3cff5eeb989d9b7750385fb
Caleb <calebgward@gmail.com>
2022-10-25 11:44:47 -0700

    working lhl_connect module

--- 3_src/modules/connect_lhl.py
+++ 3_src/modules/connect_lhl.py
@@ -1,12 +1,16 @@
+import psycopg2
+import pandas.io.sql as sqlio
+
 class lhl_delay_data:
     """
     Pull from database for Lighthouse Labs Midterm Project
     """
-    def __init__ (self, host, database, user, password):
+    def __init__ (self, host, database, user, password, seed = None):
         self.host = host
         self.database = database
         self.user = user
         self.password = password
+        self.seed = seed
 
     def connection(self):
         return psycopg2.connect(
@@ -17,21 +21,20 @@ class lhl_delay_data:
         )
 
     def sample_table(self, table: str, samples:int):
+        
         if table not in ['flights','passengers','fuel_comsumption']:
-            print('Error: {table} is not recognized as a table name.')
+            print(f'Error: {table} is not recognized as a table name.')
 
         cur = self.connection().cursor()
-        query = f"""
-        SELECT *
-        FROM {table}
-        ORDER BY 
-        """
-        cur.execute()
+        query = ''
+        if self.seed is not None:
+            query += f'SELECT SETSEED({self.seed}); '
+        query += f'SELECT * FROM {table} '
+       
+        if table != 'flights':
+            query += f'WHERE {table}.unique_carrier IN (SELECT op_unique_carrier FROM flights) '
+        query += f'ORDER BY RANDOM() LIMIT {samples};'
+
+        df_return = sqlio.read_sql_query(query, self.connection())
         
-        pass
-
-    def close_conn (self):
-        self.conn.close()
-
-    def __doc__(self):
-        pass
+        return df_return
